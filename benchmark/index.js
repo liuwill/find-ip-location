e9b3dfe433549d6ebba59db308ee3e2ce23dbfec
liuwill <liuwei_will@qq.com>
2017-07-07 10:32:41 +0800

    ADD: benchmark compare

--- benchmark/index.js
+++ benchmark/index.js
@@ -1,5 +1,6 @@
 'use strict'
 
+// process.env.UV_THREADPOOL_SIZE=128
 var benchmark = require('benchmark')
 var ipQuery = require('../index')
 var sampleFilePath = __dirname + '/../data/ip.production.db'
@@ -9,18 +10,23 @@ var suite = new benchmark.Suite()
 var ip = '61.136.112.25'
 var local = '127.0.0.1'
 
+var ipFinderGlobal = ipQuery.loadDBSync(sampleFilePath)
+
 // add tests
 suite.add('sync read file', function () {
   var ipFinder = ipQuery.loadDBSync(sampleFilePath)
   ipFinder.find(ip)
   ipFinder.find(local)
+}).add('global read file', function () {
+  ipFinderGlobal.find(ip)
+  ipFinderGlobal.find(local)
 })
   // add listeners
   .on('cycle', function (event) {
-    console.log(String(event.target));
+    console.log(String(event.target))
   })
   .on('complete', function () {
-    console.log('Fastest is ' + this.filter('fastest').map('name'));
+    console.log('Fastest is ' + this.filter('fastest').map('name'))
   })
   // run async
   .run({ 'async': true })
